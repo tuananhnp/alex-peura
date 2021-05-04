95ede14caafc179163643558d6feec57d9e84e7f
tuananhnp <tuananhnp1001@gmail.com>
2021-05-04 16:23:51 +0200

    new

---
+++ js/nav.slide.down.transition.js
@@ -0,0 +1,19 @@
+$('.navbar-toggler').click(function() {
+  $('#navbar4').slideToggle(300);
+  fullpage_api.setMouseWheelScrolling(true);
+});
+
+$('.navbar-brand').click(function() {
+  $('#navbar4').slideToggle(300);
+  $('.animated-icon').toggleClass('open');
+});
+
+$('.graphic-design').click(function() {
+  $('#navbar4').slideToggle(300);
+  $('.animated-icon').toggleClass('open');
+});
+
+$('.contact-me').click(function() {
+  $('#navbar4').slideToggle(300);
+  $('.animated-icon').toggleClass('open');
+});
