61669c8f12659b888edf266903dd8eb0996ae2a4
tuananhnp <tuananhnp1001@gmail.com>
2021-05-08 09:53:57 +0200

    new

--- js/nav.slide.down.transition.js
+++ js/nav.slide.down.transition.js
@@ -13,7 +13,7 @@ $('.logo-design').click(function() {
   $('.animated-icon').toggleClass('open');
 });
 
-$('.notebook-covers').click(function() {
+$('.book-covers').click(function() {
   $('#navbar4').slideToggle(300);
   $('.animated-icon').toggleClass('open');
 });
