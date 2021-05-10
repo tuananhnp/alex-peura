8595495223edeec4384622051595cabe6469278e
tuananhnp <tuananhnp1001@gmail.com>
2021-05-10 16:46:44 +0700

    Update nav.slide.down.transition.js

--- js/nav.slide.down.transition.js
+++ js/nav.slide.down.transition.js
@@ -1,34 +1,34 @@
 $('.navbar-toggler').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   fullpage_api.setMouseWheelScrolling(true);
 });
 
 $('.navbar-brand').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   $('.animated-icon').toggleClass('open');
 });
 
 $('.logo-design').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   $('.animated-icon').toggleClass('open');
 });
 
 $('.book-covers').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   $('.animated-icon').toggleClass('open');
 });
 
 $('.valentine-cards').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   $('.animated-icon').toggleClass('open');
 });
 
 $('.gift-voucher').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   $('.animated-icon').toggleClass('open');
 });
 
 $('.miscellaneous').click(function() {
-  $('#navbar4').slideToggle(400);
+  $('#navbar4').slideToggle(250);
   $('.animated-icon').toggleClass('open');
 });
